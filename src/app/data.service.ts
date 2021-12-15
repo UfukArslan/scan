430b70022e592bbbb3a5bac3adccf1cbca33c065
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2021-12-15 16:29:37 +0100

    if

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,11 +8,25 @@ import { map } from 'rxjs/operators';
 })
 export class DataService {
 
-  private _url: string = "http://bsm-ws.herokuapp.com/documents/?_id=61b4d862b2cba600168538f4";
+  private _url: string = "http://bsm-ws.herokuapp.com/documents/?_id=61b4d862b2cba600168538f4-C";
+  private _urls: string = this._url.substring(0,67);
 
   constructor(private http: HttpClient) { }
-
+  
+  
+  // getData used for test
   getData(): Observable<any>{
-    return this.http.get(this._url)
+    console.log("getData " + this._urls);
+    return this.http.get(this._urls)
   }
+
+  // getData(): Observable<any>{
+  //   console.log("getData " + this._urls);
+  //   return this.http.get(this._urls)
+  // }
+
+  //This funciton for startScanner
+  // getData(_id): Observable<any>{
+  //   return this.http.get(this._url+_id.substing(0,24))
+  // }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Data } from '@angular/router';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { Subscriber } from 'rxjs';
 import { DataService } from '../data.service';
 
 @Component({
@@ -12,6 +13,8 @@ export class HomePage implements OnInit {
   scanActive: boolean = false;
 
   public d;
+  private versionDure: string = "e";
+  private versionScan;
  
 
   constructor(private _data: DataService) {}
@@ -41,16 +44,26 @@ export class HomePage implements OnInit {
     
     if (allowed) {
       this.scanActive = true;
-      console.log('Console.log - startScanner1');
-      this.data();
+      console.log('Console.log - startScanner');
+      // this.data();
       BarcodeScanner.hideBackground();
       
       const result = await BarcodeScanner.startScan();
+      
       
       if (result.hasContent) {
         this.scanActive = false;
         alert(result.content); //The QR content will come out here
         //Handle the data as your heart desires here
+
+        // This is true function for to make a GET with SCANNER
+        // this._data.getData(result.content)
+        //   .subscribe(_data => {
+        //     console.log(_data);
+        //     this.versionCheck(_data);
+        //   }
+        //   )
+        
       } else {
         alert('NO DATA FOUND!');
       }
@@ -68,26 +81,47 @@ export class HomePage implements OnInit {
     BarcodeScanner.stopScan();
     this.scanActive = false;
   }
+  // This function going to check the last version in the database
+  versionCheck(_d, ){
+    console.log("versionCheck " + _d);
+    console.log("versionCheck " + this.d.data[0].versions[this.d.data[0].versions.length -1].identifiant);
+    
+    this.versionScan = this.d.data[0].versions[this.d.data[0].versions.length -1].identifiant
+    console.log("versionScan " + this.versionScan);
+    console.log("versionDure " + this.versionDure);
+
+    if (this.versionScan === this.versionDure){
+      alert("ok")
+    }else{
+      alert("not")
+    }
+
+  }
   
   buttonData(){
     this._data.getData()
     // .subscribe(data => this.d = data + console.log("hello" + this.d) );
     .subscribe(_data => {
       this.d = _data;
-      console.log('Console.log - 10buttonDatat10');
-      console.log("Button data" + this.d);
+      console.log('Console.log - buttonData');
+      console.log("ButtonData " + this.d.data[0].versions[this.d.data[0].versions.length -1]._id.substring(0,24));
+
+      // With under console.log you must change url "http://bsm-ws.herokuapp.com/documents" in data.service.ts
+      //console.log("ButtonData " + this.d.data[this.d.data.length -1].versions[0]._id);
+      
+      this.versionCheck(_data)
       });
   }
 
-  data(){
-    this._data.getData()
-    // .subscribe(data => this.d = data + console.log("hello" + this.d) );
-    .subscribe(_data => {
-      this.d = _data;
-      console.log('Console.log - datat');
-      console.log(this.d);
-      });
-  }
+  // data(){
+  //   this._data.getData()
+  //   // .subscribe(data => this.d = data + console.log("hello" + this.d) );
+  //   .subscribe(_data => {
+  //     this.d = _data;
+  //     console.log('Console.log - datat');
+  //     console.log(this.d);
+  //     });
+  // }
 
  
 
